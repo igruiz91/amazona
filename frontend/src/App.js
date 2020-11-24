c3b48faffc2fca85ed7268982fbd9444fd98a871
gonisr <israel.gonzalez@padigitale.it>
2020-11-24 17:41:05 +0100

    products done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
+import Product from "./components/Product";
 import data from "./data";
 
 function App() {
-  const [lindo, setLindo] = useState(7);
   return (
     <div className='grid-container'>
       <header className='row'>
@@ -20,38 +20,7 @@ function App() {
         <div>
           <div className='row center'>
             {data.products.map((product) => (
-              <div key={product._id} className='card'>
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className='medium'
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className='card-body'>
-                  <a href='product.html'>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className='rating'>
-                    <span>
-                      <i className='fa fa-star'></i>
-                    </span>
-                    <span>
-                      <i className='fa fa-star'></i>
-                    </span>
-                    <span>
-                      <i className='fa fa-star'></i>
-                    </span>
-                    <span>
-                      <i className='fa fa-star'></i>
-                    </span>
-                    <span>
-                      <i className='fa fa-star'></i>
-                    </span>
-                  </div>
-                  <div className='price'>${product.price}</div>
-                </div>
-              </div>
+              <Product key={product._id} product={product} />
             ))}
           </div>
         </div>
